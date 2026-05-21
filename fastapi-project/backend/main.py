837fa2bf1187cfddfb50c3f0ca053ada699f0311
zsb2505030308 <3577705391@qq.com>
2026-05-21 21:42:38 +0800

    main.py

--- fastapi-project/backend/main.py
+++ fastapi-project/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI#1.1创建fastapi环境
 from fastapi .middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from openai import OpenAI
 
 app=FastAPI()#2.5允许前端跨域访问
 
@@ -12,16 +13,31 @@ app.add_middleware(
     allow_headers=["*"],
 )#2前端网页调用接口时的必需品(仅后端测试时可删)
 
+client=OpenAI(
+    api_key="opkey",
+    base_url="http://localhost:11434/v1"
+)#opkey为自定义字符串
+#Ollama本地模型的固定地址
+
+def ask_llm(question: str) -> str:
+    try:
+        response = client.chat.completions.create(
+            model="qwen:0.5b",
+            messages=[
+                {"role": "user", "content": question}
+            ]
+        )
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        return f"出错了：{str(e)}"#本地模型
+
 @app.get("/")
 def read_root():
     return {"hello":"world"}
 
 @app.get("/ping")
 def ping():
-    return {"message":"pong"}
-
-def ask_llm(question:str)->str:
-    return f"回答:{question}"#利用ai来回复用户问题
+    return {"message":"pong"} 
 
 class ChatRequest(BaseModel):
     question:str
